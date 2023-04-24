44bf1b07af33c2704256d3e4fc9e9378fcdc93b5
Denise Chan <denise.chan112@gmail.com>
2023-04-24 10:55:27 +0100

    add footer-bottom

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './Newsletter.css';
 
 const Newsletter = () => (
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div class="footer-bottom">
+    <div class="social-media">
+      <a href="#fb"><i class="social-icons fa-brands fa-facebook-f"></i></a>
+      <a href="#ig"><i class="social-icons fa-brands fa-instagram"></i></a>
+      <a href="#tt"><i class="social-icons fa-brands fa-tiktok"></i></a>
+    </div>
+    <div class="footer-links">
+      <p><span class="footer-link-text">Jerk Shack Restaurant © 2023</span>
+      <a href="#privacy-policy"><span class="footer-link-text">Privacy Policy</span></a>
+      <a href="#terms-conditions"><span class="footer-link-text">Terms & Conditions</span></a></p>
+    </div>
   </div>
 );
 
