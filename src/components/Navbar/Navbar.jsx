3db1bdee5e499e8621b1f930577f6e7072220193
Denise Chan <denise.chan112@gmail.com>
2023-04-24 09:53:25 +0100

    added google fonts and navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav id="navbar">
+      <a href="#home" class="nav-items" id="nav-home">Home</a>
+      <a href="#about" class="nav-items">About</a>
+      <a href="#menu" class="nav-items">Menu</a>
+      <a href="#bookings" class="nav-items">Bookings</a>
+      <a href="#blog" class="nav-items">Blog</a>
+      <a href="#faq" class="nav-items">FAQ</a>
+      <a href="#contact" class="nav-items">Contact</a>
+  </nav>
 );
 
 export default Navbar;
