e672eaf2a54cd71980704ea33ddacf13605a6904
Denise Chan <denise.chan112@gmail.com>
2023-04-28 10:45:59 +0100

    update navbar with a dropdown menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 import './Navbar.css';
 import { Link } from "react-router-dom";
 
-// Import react bootstrap
-import Dropdown from 'react-bootstrap/Dropdown';
-import 'bootstrap/dist/css/bootstrap.min.css';
+function Navbar() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-const Navbar = () => (
-  <div className="navbar-wrapper">
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
     <nav className="navbar">
-      <ul className="navbar-list">
-        <li className="navbar-items"><Link to="/" className="nav-items" id="nav-home">Home</Link></li>
-        <li className="navbar-items"><a href="#about" className="nav-items">About</a></li>
-        <li className="navbar-items"><a href="#menu" className="nav-items">Menu</a></li>
-        <li className="navbar-items">
-          <Dropdown>
-            <Dropdown.Toggle variant="secondary" id="bookings-dropdown">
-              Bookings
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Reservation</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Queuing</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </li>
-        <li className="navbar-items"><a href="#blog" className="nav-items">Blog</a></li>
-        <li className="navbar-items"><a href="#faq" className="nav-items">FAQ</a></li>
-        <li className="navbar-items"><Link to="/contact" className="nav-items">Contact</Link></li>
-      </ul>
+      <Link to="/" id="home">Home</Link>
+      <a href="#about">About</a>
+      <a href="#menu">Menu</a>
+      <div className="dropdown">
+        <button className="dropbtn" onClick={handleDropdownToggle}>
+          Bookings <i className="fa fa-caret-down"></i>
+        </button>
+        <div className={`dropdown-content ${isDropdownOpen ? "show" : ""}`}>
+          <a href="#reservation">Reservation</a>
+          <a href="#queuing">Queuing</a>
+        </div>
+      </div>
+      <a href="#blog">Blog</a>
+      <a href="#faq">FAQ</a>
+      <Link to="/contact">Contact</Link>
     </nav>
-  </div>
-);
+  );
+}
 
 export default Navbar;
 
