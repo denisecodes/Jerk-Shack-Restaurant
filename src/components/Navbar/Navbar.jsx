ac9b375d9423d4ad5d2661b681847f2651295423
Denise Chan <denise.chan112@gmail.com>
2023-04-28 11:25:08 +0100

    adjust padding in navbar and add comments so it's easier to understand state in navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 import './Navbar.css';
 import { Link } from "react-router-dom";
 
+
 function Navbar() {
+  // Step 1
+  // sets isDropdownOpen to false by default so that the dropdown content is not shown
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const handleDropdownToggle = () => {
@@ -15,10 +18,14 @@ function Navbar() {
       <a href="#about">About</a>
       <a href="#menu">Menu</a>
       <div className="dropdown">
+      {/* Step 2 */}
+      {/* when the user clicks on "Bookings", if the isDropdownOpen is not true(false), then it changes isDropdownOpen to true */}
         <button className="dropbtn" onClick={handleDropdownToggle}>
           Bookings <i className="fa fa-caret-down"></i>
         </button>
-        <div className={`dropdown-content ${isDropdownOpen ? "show" : ""}`}>
+        {/* Step 3 */}
+        {/* when isDropdownOpen is true, then show-content adds the dropdown-content class to the div  */}
+        <div className={`dropdown-content ${isDropdownOpen ? "show-content" : ""}`}> 
           <a href="#reservation">Reservation</a>
           <a href="#queuing">Queuing</a>
         </div>
