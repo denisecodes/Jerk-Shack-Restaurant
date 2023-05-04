2f025a40a73050062f527ab12e6fb9cc96509fcf
Denise Chan <119052310+denisecodes@users.noreply.github.com>
2023-05-04 13:31:08 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 import Contact from './pages/Contact/Contact';
 import Queuing from './pages/Queuing/Queuing';
 
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
-
-
 // Clear currentTicket number every time the web app compiles again
 localStorage.removeItem('currentTicket');
 
-
-const router = createBrowserRouter([
-{
-    path: "/",
-    element: <App />,
-},
-{
-    path: "contact",
-    element: <Contact />,
-},
-{
-    path: "queuing",
-    element: <Queuing />,
-},
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode>
-    <RouterProvider router={router} />
-</React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Route exact path="/" component={App} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/queuing" component={Queuing} />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
